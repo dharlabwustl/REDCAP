f65e7441304c8ee4b6134acece59514b96e3a886
dharlabwustl <atulkumar@wustl.edu>
2023-11-21 13:34:34 -0600

    Second Commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os,sys
-api_token='EC6A2206FF8C1D87D4035E61C99290FF' #os.environ['REDCAP_API_TOKEN']
+api_token=os.environ['REDCAP_API_TOKEN']
 config = dict(
     api_super_token = 'ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234',
     api_token       = api_token,
