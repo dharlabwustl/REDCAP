a8b08918f335f2cc89d9c37124c2ed11992de157
dharlabwustl <atulkumar@wustl.edu>
2023-12-11 14:48:41 -0600

    Second Commit

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os,sys
-api_token=os.environ['REDCAP_API_TOKEN']
-config = dict(
-    api_super_token = 'ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234',
-    api_token       = api_token,
-    api_url         = 'https://redcap.wustl.edu/redcap/api/'
-)
+# import os,sys
+# api_token=os.environ['REDCAP_API_TOKEN']
+# config = dict(
+#     api_super_token = 'ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234ABCD1234',
+#     api_token       = api_token,
+#     api_url         = 'https://redcap.wustl.edu/redcap/api/'
+# )
--- redcapapi.py
+++ redcapapi.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import requests,json
 import pandas as pd
-from config import config
-import requests, hashlib, json
+# from config import config
+import requests, hashlib, json,glob
 import os,sys,subprocess
+api_token=os.environ['REDCAP_API_TOKEN']
+api_url='https://redcap.wustl.edu/redcap/api/'
 fields = {
-    'token': config['api_token'],
+    'token':api_token, # api_token,
     'content': 'record',
     'format': 'json',
     'type': 'flat'
@@ -17,9 +19,10 @@ print(df_scan)
 df_scan.to_csv('test.csv',index=False)
 df_scan=pd.read_csv('test.csv',index_col=False, dtype=object)
 ############FILTER TO GET THE SINGLE ROW#######################
-df_scan_sample=df_scan[(df_scan['redcap_repeat_instance']=="2" ) & (df_scan['record_id']=='ATUL_001')].reset_index()
+# df_scan_sample=df_scan[(df_scan['redcap_repeat_instance']=="2" ) & (df_scan['record_id']=='ATUL_001')].reset_index()
+df_scan_sample=df_scan[(df_scan['record_id']=="1" )].reset_index()
 #######################################################################
-field_id='subject_id'
+field_id='subject'
 field_value='OURSECONDSUBJECT'
 print(df_scan_sample)
 record = {
@@ -31,31 +34,31 @@ record = {
 }
 data = json.dumps([record])
 fields = {
-    'token': config['api_token'],
+    'token': api_token,
     'content': 'record',
     'format': 'json',
     'type': 'flat',
     'data': data,
 }
-r = requests.post(config['api_url'],data=fields)
+r = requests.post(api_url,data=fields)
 print('HTTP Status: ' + str(r.status_code))
 print(r.text)
-file = '/home/atul/Downloads/COLI_HM25_CT_1_COLI_HM25_03092021_1954_2_thresh_0_40_VersionDate-11302022_01_07_2023.pdf'
-fields = {
-    'token': config['api_token'],
-    'content': 'file',
-    'action': 'import',
-    'repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
-    'repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
-    'record': str(df_scan_sample.loc[0,'record_id']),
-    'field': 'photo_as_pdf',
-    'returnFormat': 'json'
-}
-
-file_path=file
-file_obj = open(file_path, 'rb')
-r = requests.post(config['api_url'],data=fields,files={'file':file_obj})
-file_obj.close()
-
-print('HTTP Status: ' + str(r.status_code))
-print(r.text)
+# file =glob.glob('/home/atul/Downloads/*.pdf')[0] # '/home/atul/Downloads/COLI_HM25_CT_1_COLI_HM25_03092021_1954_2_thresh_0_40_VersionDate-11302022_01_07_2023.pdf'
+# fields = {
+#     'token': api_token,
+#     'content': 'file',
+#     'action': 'import',
+#     'repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
+#     'repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
+#     'record': str(df_scan_sample.loc[0,'record_id']),
+#     'field': 'photo_as_pdf',
+#     'returnFormat': 'json'
+# }
+#
+# file_path=file
+# file_obj = open(file_path, 'rb')
+# r = requests.post(api_url,data=fields,files={'file':file_obj})
+# file_obj.close()
+#
+# print('HTTP Status: ' + str(r.status_code))
+# print(r.text)
