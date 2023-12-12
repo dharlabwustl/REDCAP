ea84c844ace01c49baf0b39df79207d27ab9d63d
dharlabwustl <atulkumar@wustl.edu>
2023-12-11 18:37:45 -0600

    Second Commit

--- redcapapi.py
+++ redcapapi.py
@@ -4,8 +4,8 @@ import pandas as pd
 # from config import config
 import requests, hashlib, json,glob
 import os,sys,subprocess
-api_token=os.environ['REDCAP_API_TOKEN']
-api_url='https://redcap.wustl.edu/redcap/api/'
+api_token=sys.argv[1] #os.environ['REDCAP_API_TOKEN']
+api_url=sys.argv[2] #'https://redcap.wustl.edu/redcap/api/'
 fields = {
     'token':api_token, # api_token,
     'content': 'record',
@@ -26,8 +26,8 @@ field_id='subject'
 field_value='OURSECONDSUBJECT'
 print(df_scan_sample)
 record = {
-    'redcap_repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
-    'redcap_repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
+    # 'redcap_repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
+    # 'redcap_repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
     'record_id': str(df_scan_sample.loc[0,'record_id']),
     field_id: field_value
 
@@ -43,22 +43,22 @@ fields = {
 r = requests.post(api_url,data=fields)
 print('HTTP Status: ' + str(r.status_code))
 print(r.text)
-# file =glob.glob('/home/atul/Downloads/*.pdf')[0] # '/home/atul/Downloads/COLI_HM25_CT_1_COLI_HM25_03092021_1954_2_thresh_0_40_VersionDate-11302022_01_07_2023.pdf'
-# fields = {
-#     'token': api_token,
-#     'content': 'file',
-#     'action': 'import',
-#     'repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
-#     'repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
-#     'record': str(df_scan_sample.loc[0,'record_id']),
-#     'field': 'photo_as_pdf',
-#     'returnFormat': 'json'
-# }
-#
-# file_path=file
-# file_obj = open(file_path, 'rb')
-# r = requests.post(api_url,data=fields,files={'file':file_obj})
-# file_obj.close()
-#
-# print('HTTP Status: ' + str(r.status_code))
-# print(r.text)
+file =glob.glob('/home/atul/Downloads/*.pdf')[0] # '/home/atul/Downloads/COLI_HM25_CT_1_COLI_HM25_03092021_1954_2_thresh_0_40_VersionDate-11302022_01_07_2023.pdf'
+fields = {
+    'token': api_token,
+    'content': 'file',
+    'action': 'import',
+    # 'repeat_instrument':str(df_scan_sample.loc[0,'redcap_repeat_instrument']),
+    # 'repeat_instance':str(df_scan_sample.loc[0,'redcap_repeat_instance']),
+    'record': str(df_scan_sample.loc[0,'record_id']),
+    'field': 'session_pdf' , #'photo_as_pdf',
+    'returnFormat': 'json'
+}
+
+file_path=file
+file_obj = open(file_path, 'rb')
+r = requests.post(api_url,data=fields,files={'file':file_obj})
+file_obj.close()
+
+print('HTTP Status: ' + str(r.status_code))
+print(r.text)
